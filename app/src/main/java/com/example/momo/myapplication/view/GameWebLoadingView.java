956f5b8a4742bd2163d2d469b7f1f613986e5c02
wang.renguang <wang.renguang@immomo.com>
2017-08-14 10:23:05 +0800

    3d loading view

--- app/src/main/java/com/example/momo/myapplication/view/GameWebLoadingView.java
+++ app/src/main/java/com/example/momo/myapplication/view/GameWebLoadingView.java
@@ -3,9 +3,12 @@ package com.example.momo.myapplication.view;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
-import android.support.v7.widget.AppCompatImageView;
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.momo.myapplication.R;
 
@@ -13,12 +16,20 @@ import com.example.momo.myapplication.R;
  * Created by wangrenguang on 2017/8/5.
  */
 
-public class GameWebLoadingView extends AppCompatImageView {
+public class GameWebLoadingView extends ImageView implements ValueAnimator.AnimatorUpdateListener {
 
-    private final int[] mBgRes = {R.drawable.game_web_loading_01, R.drawable.game_web_loading_02, R.drawable
-            .game_web_loading_03};
-    private ObjectAnimator objectAnimator;
+    private static final float CHANGE_OFFSET = 0.05f;
+
+    private final int[] mBgRes = {R.drawable
+            .game_web_loading_03, R.drawable.game_web_loading_01, R.drawable.game_web_loading_02};
+    private ValueAnimator valueAnimator;
     private int mCurrentIndex = 0;
+
+    private Camera mCamera;
+    private Matrix cameraMatrix;
+
+    private float changeDegree = 90;
+    private float rotateDegree;
 
     public GameWebLoadingView(Context context) {
         super(context);
@@ -36,38 +47,62 @@ public class GameWebLoadingView extends AppCompatImageView {
     }
 
     private void init() {
-        objectAnimator = ObjectAnimator.ofInt(this, "rotationY", 0, 360);
-        objectAnimator.setDuration(1000);
-        objectAnimator.start();
-        objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
-        objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float value = (float) animation.getAnimatedValue();
-                if (mCurrentIndex >= mBgRes.length) {
-                    mCurrentIndex = 0;
-                }
-                if (value == 180) {
-                    GameWebLoadingView.this.setImageResource(mBgRes[mCurrentIndex]);
-                }
-            }
-        });
+
+        mCamera = new Camera();
+        cameraMatrix = new Matrix();
+
+        valueAnimator = ObjectAnimator.ofFloat(0, 180);
+        valueAnimator.setDuration(1500);
+        valueAnimator.start();
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.setRepeatMode(ValueAnimator.RESTART);
+        valueAnimator.addUpdateListener(this);
 
     }
 
-    public void rotationY(int rotationX) {
-        super.setRotationX(rotationX);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        mCamera.save();
+        mCamera.rotateY(rotateDegree);
+        mCamera.getMatrix(cameraMatrix);
+        mCamera.restore();
+        final int cx = getWidth() >> 1;
+        final int cy = getHeight() >> 1;
+        cameraMatrix.preTranslate(-cx, -cy);
+        cameraMatrix.postTranslate(cx, cy);
+
+        canvas.concat(cameraMatrix);
+        super.onDraw(canvas);
+        canvas.restore();
     }
 
     @Override
     public void setVisibility(int visibility) {
         super.setVisibility(visibility);
         if (visibility == View.VISIBLE) {
-            objectAnimator.start();
+            valueAnimator.start();
         } else {
-            objectAnimator.end();
+            valueAnimator.end();
         }
     }
 
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation) {
+        final float value = (float) animation.getAnimatedValue();
+        final float oldDegree = rotateDegree;
+        rotateDegree = value;
+        if (rotateDegree >= 90 && rotateDegree <= 180) {
+            rotateDegree += 180;
+        }
+        final float changeDegree = this.changeDegree * (1 - CHANGE_OFFSET);
+        if (oldDegree < changeDegree && value >= changeDegree) {
+            if (mCurrentIndex >= mBgRes.length) {
+                mCurrentIndex = 0;
+            }
+            GameWebLoadingView.this.setImageResource(mBgRes[mCurrentIndex]);
+            mCurrentIndex++;
+        }
+        invalidate();
+    }
 }
