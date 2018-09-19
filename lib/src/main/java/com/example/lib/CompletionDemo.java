41bd4b9dae64f0911edcddbb06cc7de56f34b7ed
wangrenguang <w583943383@foxmail.com>
2018-09-19 09:51:50 +0800

    并发demo

---
+++ lib/src/main/java/com/example/lib/CompletionDemo.java
@@ -0,0 +1,74 @@
+package com.example.lib;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class CompletionDemo {
+
+    private final ExecutorService executorService;
+
+    public CompletionDemo(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
+
+
+    void renderPage(CharSequence source) {
+        List<ImageInfo> info = scanForImage(source);//scanforimage
+
+        CompletionService<String> completionService = new ExecutorCompletionService<>(executorService);
+        for (final ImageInfo imageInfo : info) {
+            completionService.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return imageInfo.downloadImage();
+                }
+            });
+        }
+        renderText(source);
+
+        for (int i = 0; i < info.size(); i++) {
+            try {
+                Future<String> future = completionService.take();
+                String image = future.get();
+                renderImage(image);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    private void renderText(CharSequence source) {
+        // 绘制文本
+    }
+
+    private void renderImage(String image) {
+        // 绘制图片
+    }
+
+    private List<ImageInfo> scanForImage(CharSequence source) {
+        return new ArrayList<>();
+    }
+
+
+    private class ImageInfo {
+
+        public String downloadImage() {
+            return "";
+        }
+    }
+
+
+    public static void main(String[] a) {
+
+    }
+}
---
+++ lib/src/main/java/com/example/lib/MemoizerDemo.java
@@ -0,0 +1,55 @@
+package com.example.lib;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class MemoizerDemo {
+
+    public interface Computable<A, V> {
+        V compute(A arg) throws InterruptedException;
+    }
+
+
+    public class Memoizer<A, V> implements Computable<A, V> {
+
+        private final Map<A, FutureTask<V>> cache = new ConcurrentHashMap<A, FutureTask<V>>();
+        private final Computable<A, V> computable;
+
+
+        public Memoizer(Computable<A, V> computable) {
+            this.computable = computable;
+        }
+
+
+        @Override
+        public V compute(final A arg) throws InterruptedException {
+            while (true) {
+                // todo 限制循环重试次数  缓存时间设置  缓存有效期设置
+                FutureTask<V> f = cache.get(arg);
+                if (f == null) {
+                    f = new FutureTask<V>(new Callable<V>() {
+                        @Override
+                        public V call() throws Exception {
+                            return computable.compute(arg);
+                        }
+                    });
+                    cache.putIfAbsent(arg, f);
+                    f.run();
+                }
+                try {
+                    return f.get();
+                } catch (CancellationException e) {
+                    cache.remove(arg, f);
+                } catch (ExecutionException e) {
+                    throw new IllegalStateException("", e);
+                }
+            }
+
+        }
+    }
+
+}
