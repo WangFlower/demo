debf8501890d2e183e303664af590079e805c156
jugg.rg <wang.renguang@immomo.com>
2019-02-23 12:23:18 +0800

    work test2

--- app/src/main/java/com/example/momo/myapplication/gittest/GitTest.java
+++ app/src/main/java/com/example/momo/myapplication/gittest/GitTest.java
@@ -1,8 +1,14 @@
 package com.example.momo.myapplication.gittest;
 
+import android.util.Log;
+
 /**
  * @author wang.renguang
  * @time 2019/2/23
  */
 public class GitTest {
+
+    public void ff() {
+        Log.i("wangrenguang", "onOffsetChanged ");
+    }
 }
