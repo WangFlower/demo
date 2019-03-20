fdc8a3e351466075f4d6e93fe18b4eaba43eccc4
jugg.rg <wang.renguang@immomo.com>
2019-03-20 20:17:59 +0800

    tree

--- lib/src/main/java/com/example/lib/structure/tree/SortTree.java
+++ lib/src/main/java/com/example/lib/structure/tree/SortTree.java
@@ -2,6 +2,9 @@ package com.example.lib.structure.tree;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 排序二叉树
  */
@@ -52,9 +55,78 @@ public class SortTree<V extends Comparable<V>> {
 
 
     private void delete(TreeNode treeNode) {
-        if (treeNode.left == null) {
-            treeNode.parent.
+
+        if (treeNode == root) {
+            // 删除根节点
+            root = null;
+            return;
         }
+
+        if (treeNode.left == null && treeNode.right == null) {
+            // 没有左右子树的情况
+            if (treeNode == treeNode.parent.left) {
+                treeNode.parent.left = null;
+            } else {
+                treeNode.parent.right = null;
+            }
+            treeNode.parent = null;
+        } else if (treeNode.left == null) {
+            // 只有右子树的情况
+            if (treeNode == treeNode.parent.left) {
+                treeNode.parent.left = treeNode.right;
+            } else {
+                treeNode.parent.right = treeNode.right;
+            }
+            treeNode.right.parent = treeNode.parent;
+        } else if (treeNode.right == null) {
+            // 只有左子树
+            if (treeNode == treeNode.parent.left) {
+                treeNode.parent.left = treeNode.left;
+            } else {
+                treeNode.parent.right = treeNode.left;
+            }
+            treeNode.left.parent = treeNode.parent;
+        } else {
+            // 左子树都有
+
+            // 找到右子树中最小的节点
+            TreeNode node = treeNode.right;
+            while (node.left != null) {
+                node = node.left;
+            }
+
+            if (treeNode == treeNode.parent.left) {
+                treeNode.parent.left = node;
+            } else {
+                treeNode.parent.right = node;
+            }
+            node.parent = treeNode.parent;
+            node.left = treeNode.left;
+            node.right = treeNode.right;
+            treeNode.parent = null;
+            treeNode.left = null;
+            treeNode.right = null;
+        }
+    }
+
+
+    public void print() {
+        // 中序遍历得到有序序列
+        List<V> data = print(root);
+
+    }
+
+
+    private List<V> print(TreeNode treeNode) {
+        List<V> result = new ArrayList<>();
+        if (treeNode.left != null) {
+            result.addAll(print(treeNode.left));
+        }
+        result.add(treeNode.value);
+        if (treeNode.right != null) {
+            result.addAll(print(treeNode.right));
+        }
+        return result;
     }
 
 
